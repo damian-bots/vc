f64c6276b2eaf2c8aae1bbb6f8fe7a74cb37fa06
damian-bots <tosu0791@gmail.com>
2024-12-23 01:58:37 +0530

    Update config.py

--- VCPlayBot/config.py
+++ VCPlayBot/config.py
@@ -24,7 +24,7 @@ SOURCE_CODE = getenv("SOURCE_CODE", "github.com/QueenArzoo/VCPlayBot")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
-LOG_GRP = getenv("LOG_GRP", None)
+LOG_GRP = getenv("LOG_GRP", -1002085443861)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5096741943").split()))
